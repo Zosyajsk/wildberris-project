b18b9e83a09d4faf8a024f97072ce5de01cbbe01
zazholdas <zosyajsk@mail.ru>
2026-05-21 21:53:45 +0500

    check ..

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,31 +1,26 @@
 const getGoods = () => {
   const links = document.querySelectorAll(".navigation-link");
 
-  const getData = () => {
+  const getData = (value, category) => {
     fetch("/db/db.json")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        const array = category
+          ? data.filter((item) => item[category] === value)
+          : data;
+        localStorage.setItem("goods", JSON.stringify(array));
       });
   };
 
   links.forEach((link) => {
     link.addEventListener("click", (event) => {
       event.preventDefault();
-      getData();
+      const linkValue = link.textContent;
+      const category = link.dataset.field;
+
+      getData(linkValue, category);
     });
   });
-
-  //   localStorage.setItem("goods", JSON.stringify([1, 2, 3, 4]));
-  //   const goods = JSON.parse(localStorage.getItem("goods"));
-  //   console.log(localStorage);
-
-  //   localStorage.removeItem("goods");
-  //   console.log(localStorage);
-
-  localStorage.setItem("data", JSON.stringify("getData"));
-  const data = JSON.parse(localStorage.getItem("data"));
-  console.log(localStorage);
 };
 
 getGoods();
